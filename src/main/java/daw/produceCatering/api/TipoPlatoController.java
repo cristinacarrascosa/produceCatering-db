baec42164ac02aa151c7048a5cf29689508f2726
cristinacarrascosa <cctausias@gmail.com>
2022-12-12 12:09:22 +0100

    creados todos los controllers

---
+++ src/main/java/daw/produceCatering/api/TipoPlatoController.java
@@ -0,0 +1,10 @@
+package daw.produceCatering.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tipoplato")
+public class TipoPlatoController {
+    
+}
