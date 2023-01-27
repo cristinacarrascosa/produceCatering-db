8ebf66cf623f047a9db498f63dcaa1abab0a07e5
Cristina Carrascosa <cctausias@gmail.com>
2023-01-27 19:27:34 +0100

    trabajando en getPage lineascandallo

--- src/main/java/daw/produceCatering/api/LineaEscandallo.java
+++ src/main/java/daw/produceCatering/api/LineaEscandallo.java
@@ -3,8 +3,12 @@ package daw.produceCatering.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,17 +20,33 @@ import daw.produceCatering.service.LineaEscandalloService;
 public class LineaEscandallo {
 
     @Autowired
-    LineaEscandalloService oLineaEscanadalloService;
+    LineaEscandalloService oLineaEscandalloService;
 
     @GetMapping("/{id}")
     public ResponseEntity<LineaEscandalloEntity> get(@PathVariable(value = "id") Long id) {
-        return new ResponseEntity<LineaEscandalloEntity>(oLineaEscanadalloService.get(id), HttpStatus.OK);
+        return new ResponseEntity<LineaEscandalloEntity>(oLineaEscandalloService.get(id), HttpStatus.OK);
     }
 
     @GetMapping("/count")
     public ResponseEntity<Long> count() {
-        return new ResponseEntity<Long>(oLineaEscanadalloService.count(), HttpStatus.OK);
+        return new ResponseEntity<Long>(oLineaEscandalloService.count(), HttpStatus.OK);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Long> delete(@PathVariable(value = "id") Long id) {
+        return new ResponseEntity<Long>(oLineaEscandalloService.delete(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Long> create(@RequestBody LineaEscandalloEntity oLineaEscandalloEntity) {
+        return new ResponseEntity<Long>(oLineaEscandalloService.create(oLineaEscandalloEntity), HttpStatus.OK);
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<Long> update(@RequestBody LineaEscandalloEntity oLineaEscandalloEntity) {
+        return new ResponseEntity<Long>(oLineaEscandalloService.update(oLineaEscandalloEntity), HttpStatus.OK);
+    }
+
 
     
 
--- src/main/java/daw/produceCatering/service/LineaEscandalloService.java
+++ src/main/java/daw/produceCatering/service/LineaEscandalloService.java
@@ -1,10 +1,16 @@
 package daw.produceCatering.service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import daw.produceCatering.entity.LineaEscandalloEntity;
 import daw.produceCatering.exception.ResourceNotFoundException;
+import daw.produceCatering.exception.ResourceNotModifiedException;
+import daw.produceCatering.helper.ValidationHelper;
 import daw.produceCatering.repository.LineaEscandalloRepository;
 
 @Service
@@ -22,11 +28,7 @@ public class LineaEscandalloService {
     @Autowired
     AuthService oAuthService;
 
-    public void validate(Long id) {
-        if (!oLineaEscandalloRepository.existsById(id)) {
-            throw new ResourceNotFoundException("id " + id + " not exist");
-        }
-    }
+    
 
    
     public LineaEscandalloEntity get(Long id) {
@@ -40,6 +42,69 @@ public class LineaEscandalloService {
         //oAuthService.OnlyAdmins();
         return oLineaEscandalloRepository.count();
     }
+
+    public Page<LineaEscandalloEntity> getPage(Pageable oPageable, Long lEscandallo, Long lReferencia) {
+        ValidationHelper.validateRPP(oPageable.getPageSize());
+        Page<LineaEscandalloEntity> oPage = null;
+        if (lEscandallo == null) 
+            
+                oPage = oLineaEscandalloRepository.findAll(oPageable);
+            
+    
+            
+       
+        return oPage;
+    }
+
+    public Long create(LineaEscandalloEntity oLineaEscandalloEntity) {
+        validate(oLineaEscandalloEntity);
+        oLineaEscandalloEntity.setId(0L);
+        oEscandalloService.validate(oLineaEscandalloEntity.getEscandallo().getId());
+        oReferenciaService.validate(oLineaEscandalloEntity.getReferencia().getId());
+        oLineaEscandalloEntity.setEscandallo(oEscandalloService.get(oLineaEscandalloEntity.getEscandallo().getId()));
+        oLineaEscandalloEntity.setReferencia(oReferenciaService.get(oLineaEscandalloEntity.getReferencia().getId()));
+        oLineaEscandalloEntity.setId(null);
+        return oLineaEscandalloRepository.save(oLineaEscandalloEntity).getId();
+    }
+
+
+    public void validate(Long id) {
+        if (!oLineaEscandalloRepository.existsById(id)) {
+            throw new ResourceNotFoundException("id " + id + " not exist");
+        }
+    }
+
+    public void validate(LineaEscandalloEntity oLineaEscandalloEntity) {
+        oEscandalloService.validate(oLineaEscandalloEntity.getEscandallo().getId());
+        oReferenciaService.validate(oLineaEscandalloEntity.getReferencia().getId());
+    }
+
+    public Long delete(Long id) {
+        // falta añadir onlyAdmin si hace falta
+        if (oLineaEscandalloRepository.existsById(id)) {
+            oLineaEscandalloRepository.deleteById(id);
+            if (oLineaEscandalloRepository.existsById(id)) {
+                throw new ResourceNotModifiedException("id " + id + " no se ha podido borrar");
+            } else {
+                return id;
+            }
+
+        } else {
+            throw new ResourceNotFoundException("id " + id + " no existe");
+        }
+    }
+
+    //@Transactional
+    public Long update(LineaEscandalloEntity oLineaEscandalloEntity) {
+        validate(oLineaEscandalloEntity.getId());
+        validate(oLineaEscandalloEntity);
+        oEscandalloService.validate(oLineaEscandalloEntity.getEscandallo().getId());
+        oReferenciaService.validate(oLineaEscandalloEntity.getReferencia().getId());
+        oLineaEscandalloEntity.setEscandallo(oEscandalloService.get(oLineaEscandalloEntity.getEscandallo().getId()));
+        oLineaEscandalloEntity.setReferencia(oReferenciaService.get(oLineaEscandalloEntity.getReferencia().getId()));
+        return oLineaEscandalloRepository.save(oLineaEscandalloEntity).getId();
+    }
+
 
     
 
